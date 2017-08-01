d1d60a2aa5f942bcc7265fdcda6dfba594f26bf6
Marcelo Boeira <me@marceloboeira.com>
2017-08-01 22:27:26 +0200

    feature: Add POST /upload endpoint
    POST /upload
    
    Every time a users uploads a batch of panoramas, this endpoint will be called.
    
    e.g.:
    
    body { "count": 3, "timestamp": 12890212 }
    
    where:
    
    * count - Number of uploaded panoramas
    * timestamp - Epoch time of upload in UTC
    
    Returns
    
    Empty body with either 201 or 204
    
    * 201 - in case of success
    * 204 - timestamp if older than 60 seconds

--- src/main/scala/com/memoria/Server.scala
+++ src/main/scala/com/memoria/Server.scala
@@ -1,16 +1,31 @@
 package com.memoria
 
+import java.time.Instant
+
+import scala.collection.mutable
 import com.twitter.util.Await
 import com.twitter.finagle.Http
-
+import com.twitter.finagle.http.Status
 import io.finch._
 import io.finch.circe._
 import io.circe.generic.auto._
 
-object Server extends App {
-   def root: Endpoint[String] = get(*) {
-     Ok("Memoria")
-   }
+case class Upload(count: Int, timestamp: Long)
 
-   Await.ready(Http.server.serve(":9000", root.toService))
+object Server extends App {
+  def root: Endpoint[String] = get(*) {
+    Ok("Memoria")
+  }
+
+  def postUpload: Endpoint[Unit] = post("upload" :: jsonBody[Upload]) { upload: Upload =>
+    Option(ageOf(upload.timestamp)).filter(_ <= 60) match {
+      case Some(x) => Output.unit(Status.Created)
+      case None => Output.unit(Status.NoContent)
+    }
+  }
+
+  private def ageOf(timestamp : Long): Long = { val a = now - timestamp; a }
+  private def now: Long = { Instant.now.getEpochSecond }
+
+  Await.ready(Http.server.serve(":9000", (root :+: postUpload).toService))
 }
--- src/test/scala/com/memoria/ServerTest.scala
+++ src/test/scala/com/memoria/ServerTest.scala
@@ -1,19 +1,65 @@
 package com.memoria
 
+import java.time.Instant
+
 import com.twitter.finagle.http.Status
+import com.twitter.util.Throw
+import io.finch.Error.NotParsed
 import io.finch.Input
-import org.scalatest.{FunSpec, Matchers}
+import io.finch.items.BodyItem
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FunSpec, Matchers}
 
-class ServerTest extends FunSpec with Matchers {
+class ServerTest extends FunSpec with Matchers with BeforeAndAfterEach {
   import com.memoria.Server.root
+  import com.memoria.Server.postUpload
 
-  describe ("root endpoint") {
-    it("returns 200") {
-      root(Input.get("/")).awaitOutputUnsafe().map(_.status) shouldBe Some(Status.Ok)
+  def uploadInput(count : Int, secondsAgo : Int): Input = {
+    Input.post("/upload")
+      .withBody("{ \"count\": %s, \"timestamp\": %s }"
+        .format(count, Instant.now.getEpochSecond - secondsAgo))
+  }
+
+  def invalidUploadInput: Input = {
+    Input.post("/upload")
+      .withBody("{ \"invalid\": 1, \"timestamp\": 10000 }")
+  }
+
+  describe ("GET /") {
+    it("returns 200 - Success") {
+      root(Input.get("/"))
+        .awaitOutputUnsafe()
+        .map(_.status) shouldBe Some(Status.Ok)
     }
 
     it("returns 'Memoria' in the body") {
-      root(Input.get("/")).awaitValueUnsafe() shouldBe Some("Memoria")
+      root(Input.get("/"))
+        .awaitValueUnsafe() shouldBe Some("Memoria")
+    }
+  }
+
+  describe("POST /upload") {
+    describe("when the parameters are valid") {
+      describe("and the timestamp is not older than 60 seconds") {
+        it("returns 201 - Created") {
+          postUpload(uploadInput(1, 5))
+            .awaitOutputUnsafe()
+            .map(_.status) shouldBe Some(Status.Created)
+        }
+      }
+
+      describe("and the timestamp is older than 60 seconds") {
+        it("returns 204 - No Content") {
+          postUpload(uploadInput(1, 65))
+            .awaitOutputUnsafe()
+            .map(_.status) shouldBe Some(Status.NoContent)
+        }
+      }
+    }
+
+    describe("when the parameters are not valid") {
+      it("returns with status 400 - Bad Request") {
+        a[NotParsed] shouldBe thrownBy(postUpload(invalidUploadInput).awaitValueUnsafe())
+      }
     }
   }
 }
