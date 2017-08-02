11d55e98ed9f7b67bc410d45fe33cfe9a7bf21d5
Marcelo Boeira <me@marceloboeira.com>
2017-08-02 20:49:09 +0200

    refactor: Move cache to its own file

---
+++ src/main/scala/com/memoria/Cache.scala
@@ -0,0 +1,24 @@
+package com.memoria
+
+import java.time.Instant
+import java.util.concurrent.LinkedBlockingQueue
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Try
+
+object Cache {
+  val queue = new LinkedBlockingQueue[Upload]
+  private[this] val memory: ArrayBuffer[Upload] = ArrayBuffer.empty[Upload]
+  var uploadStatistics: UploadStatistics = UploadStatistics(0,0,0,0,0)
+
+  def add(item: Upload): Unit = { memory += item }
+  def destroyAll: Unit = synchronized { memory.clear }
+  def max: Int = { Try(memory.map(_.count).max).toOption.getOrElse(0) }
+  def min: Int = { Try(memory.map(_.count).min).toOption.getOrElse(0) }
+  def count: Int = { memory.length }
+  def sum: Int = { memory.map(_.count).sum }
+  def average: Double = { Try((sum / count).toDouble).toOption.getOrElse(0.0) }
+  def refreshStatistics = { uploadStatistics = UploadStatistics(count, sum, min, max, average) }
+  def maxAge: Long = { Instant.now.getEpochSecond - 60 }
+  def removeOldEntries: Unit = synchronized {  memory --= memory.filter(_.timestamp < maxAge) }
+}
--- src/main/scala/com/memoria/Server.scala
+++ src/main/scala/com/memoria/Server.scala
@@ -18,23 +18,6 @@ import scala.collection.mutable.ArrayBuffer
 case class Upload(count: Int, timestamp: Long)
 case class UploadStatistics(count: Int, sum: Int, min: Int, max: Int, avg: Double)
 
-object Cache {
-  val queue = new LinkedBlockingQueue[Upload]
-  private[this] val memory: ArrayBuffer[Upload] = ArrayBuffer.empty[Upload]
-  var uploadStatistics: UploadStatistics = UploadStatistics(0,0,0,0,0)
-
-  def add(item: Upload): Unit = { memory += item }
-  def destroyAll: Unit = synchronized { memory.clear }
-  def max: Int = { Try(memory.map(_.count).max).toOption.getOrElse(0) }
-  def min: Int = { Try(memory.map(_.count).min).toOption.getOrElse(0) }
-  def count: Int = { memory.length }
-  def sum: Int = { memory.map(_.count).sum }
-  def average: Double = { Try((sum / count).toDouble).toOption.getOrElse(0.0) }
-  def refreshStatistics = { uploadStatistics = UploadStatistics(count, sum, min, max, average) }
-  def maxAge: Long = { Instant.now.getEpochSecond - 60 }
-  def removeOldEntries: Unit = synchronized {  memory --= memory.filter(_.timestamp < maxAge) }
-}
-
 object Server extends App {
   val cores = 2
   val pool = Executors.newFixedThreadPool(cores)
